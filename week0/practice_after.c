58143eea7592f6562a9afe029059cf128ffb0254
guanchunh <jamie19990210@gmail.com>
2022-07-02 15:03:43 +0800

    add week0 practice_after

---
+++ week0/practice_after.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    float sum = 0.0f, corr = 0.0f; /* corrective value for rouding error */
+    for (int i = 0; i < 10000; i++){
+        float y = (i+1) - corr; /* add the correction to specific item */
+        float t = sum + y ; /*bits might be lost */
+        corr = (t-sum) - y ; /* recover lost bits */
+        sum = t;
+    }
+    printf("Sum: %f\n", sum);
+    return 0;
+}
