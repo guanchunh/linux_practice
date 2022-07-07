e4a587eed6ab63d070bc9b8bcc9ecc073953e070
guanchunh <jamie19990210@gmail.com>
2022-07-07 09:04:39 +0800

    add week1 overflow

---
+++ week1/overflow.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdio.h>
+void show_info(double d, int16_t n) {
+    n = (int16_t) d;
+    printf("d = %f, n = %d \n", d, n);
+}
+int main() {
+    double d1 = 32767.0, d2 = 32768.0;
+    int16_t n = 0;
+    show_info(d1, n); show_info(d2, n);
+    return(0);
+}
