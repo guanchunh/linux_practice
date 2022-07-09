d4e6a82d23f80df847a2b01a47dba19ba1889dc9
guanchunh <jamie19990210@gmail.com>
2022-07-09 13:22:17 +0800

    modify wee1 point_to_point

--- week1/point_to_point.c
+++ week1/point_to_point.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int B = 2;
-void func(int *p) { p = &B; }
+void func(int **p) { *p = &B; } //plus the star is the original life time
 int main() {
     int A = 1, C = 3;
     int *ptrA = &A;
-    func(ptrA);
+    func(&ptrA);
     printf("%d\n", *ptrA);
     return 0;
 }
