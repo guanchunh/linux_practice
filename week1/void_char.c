13e46852c0eb6bf3f7d2d0b3ae067f9945d86ed6
guanchunh <jamie19990210@gmail.com>
2022-07-09 13:02:10 +0800

    modify week1 void_char

--- week1/void_char.c
+++ week1/void_char.c
@@ -3,5 +3,5 @@ int main()
     char *X = 0;
     void *Y = 0;
     char c = *X;
-    char d = *Y; //void has no clear space and cannot be taken out
+    char d = *(char *)Y; //void has no clear space and cannot be taken out
 }
